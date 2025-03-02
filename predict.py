d04bb88e2191389b5b5bd13138a33ef12a07dfd5
tairan-w <67823130+tairan-w@users.noreply.github.com>
2025-03-02 15:19:24 +0300

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+from chemprop.parsing import parse_train_args, modify_train_args
+from chemprop.train import make_predictions
+
+if __name__ == '__main__':
+    args = parse_train_args()
+    args.checkpoint_dir = './ckpt'
+    modify_train_args(args)
+    
+    df = pd.read_csv(args.data_path)
+    pred, smiles = make_predictions(args, df.smiles.tolist())
+    df = pd.DataFrame({'smiles':smiles})
+    for i in range(len(pred[0])):
+        df[f'pred_{i}'] = [item[i] for item in pred]
+    df.to_csv(f'./predict.csv', index=False)
